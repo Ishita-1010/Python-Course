d4583a1efe78980c47cf451128a6cb24140baeea
Ishita Mukherjee <ishitamukherjee10@gmail.com>
2025-12-16 00:33:06 +0530

    Add Week2.py

---
+++ Week 2/Week2.py
@@ -0,0 +1,61 @@
+tup = ()
+print(tup)
+
+li = [1,2,3,4,5]
+print(tuple(li))
+
+tup = tuple('Geeks')
+print(tup)
+
+tup = ('Ishita')
+n = 5
+
+for i in range(int(n)):
+    tup = (tup,)
+    print(tup)
+
+tup = ("Geeks", "For", "Geeks")
+
+a,b,c = tup
+
+print(a,b,c)
+print(a)
+print(b)
+print(c)
+
+a = [1,2,3,(4,5),6,7]
+
+c = next((i for i, x in enumerate(a) if isinstance(x,tuple)),len(a))
+
+print(c)
+
+from collections import namedtuple
+import collections
+
+Student = namedtuple('Student', ['name', 'age', 'DOB'])
+
+S = Student('Ishita', '31', '10101994')
+
+print("The student age using index is:", end="")
+print(S[1])
+
+print("The studen name using keyname is:", end="")
+print(S.name)
+
+print("The student age using getattr():", end="")
+print(getattr(S, 'DOB'))
+
+Student = collections.namedtuple('Student', ['name', 'age', 'DOB'])
+
+S = Student('Ishita', '31', '10101994')
+
+li = ["Sourabh", '34', '18121990']
+
+di = {'name': "Ishani", 'age': 37, 'DOB': '25121988'}
+
+print(Student._make(li))
+print(S._asdict())
+print(Student(**di))
+
+
+
