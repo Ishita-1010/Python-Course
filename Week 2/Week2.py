3cb0d16652a2c21e7fae069fd8a890fe7935dc28
Ishita Mukherjee <ishitamukherjee10@gmail.com>
2025-12-16 22:56:32 +0530

    Update code changes

--- Week 2/Week2.py
+++ Week 2/Week2.py
@@ -57,5 +57,61 @@ print(Student._make(li))
 print(S._asdict())
 print(Student(**di))
 
+a = (1,2,3,4,5)
+b = (100,200,300,400,500)
 
+print(f"Before Swap a is {a}, b is {b}")
 
+a,b = b,a
+
+print(f"After Swap a is {a}, b is {b}")
+
+li = [5,7,4,7,4,3,5]
+
+li.sort()
+
+print(li)
+
+li.sort(reverse=True)
+
+print(li)
+
+li = list(set(li))
+
+print(li)
+
+li = list(dict.fromkeys(li))
+
+print(li)
+
+a = [2,4,2,8,1,4,6]
+
+res = []
+
+[res.append(val) for val in a if val not in res]
+
+res.sort()
+
+print(res)
+
+res = []
+
+for val in a:
+    if val not in res:
+        res.append(val)
+
+print(res)
+
+string = input("Enter a string:")
+
+vowel = 0
+consonant = 0 
+
+for i in string:
+    if i in ('a','e','i','o','u','A','E','I','O','U'):
+        vowel+=1
+    elif i.isalpha():
+        consonant+=1
+
+print(string)
+print("Vowels:", vowel, "Consonant:", consonant)
